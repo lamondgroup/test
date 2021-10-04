8c73e1e71509a714d83b76b1902b3281f51cb270
lamondgroup <91800970+lamondgroup@users.noreply.github.com>
2021-10-04 20:21:23 +0100

    fixing netlify cms

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/assets/css/theme.css');
     eleventyConfig.addPassthroughCopy('./src/assets')
+    eleventyConfig.addPassthroughCopy('./src/admin')
 
     return {
       dir: {
